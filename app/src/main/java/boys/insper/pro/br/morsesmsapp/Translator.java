77734058131227a1ef1de6aa30b47f07b00fb1a5
juanjorgegarcia <juan.j.garcia007@gmail.com>
2018-05-02 16:59:16 -0300

    Traducao do morse melhorada

--- app/src/main/java/boys/insper/pro/br/morsesmsapp/Translator.java
+++ app/src/main/java/boys/insper/pro/br/morsesmsapp/Translator.java
@@ -58,184 +58,35 @@ public class Translator {
 
     // ESTE CONSTRUTOR DEVE SER PREENCHIDO DE ACORDO COM O ENUNCIADO!
     public Translator() {
-        map = new HashMap<Character, Node>();
-        root = new Node ();
 
-        Node e = new Node('e');
-        root.setLeftChild(e);
-        Node t = new Node('t');
-        root.setRightChild(t);
-        this.map.put(' ',root);
+        this.root = new Node();
+        this.map = new HashMap<Character, Node>();
 
+        String alphabet = "_etianmsurwdkgohvf_l_pjbxcyzq__54_3___2_______16_______7___8_90";
+        Node[] nodes = new Node[alphabet.length()];
 
-        t.setParent(root);
-        Node n = new Node('n');
-        t.setLeftChild(n);
-        Node m = new Node('m');
-        t.setRightChild(m);
-        n.setParent(t);
-        Node d = new Node('d');
-        n.setLeftChild(d );
-        Node k = new Node('k');
-        n.setRightChild(k );
-        d.setParent(n);
-        Node b = new Node('b');
-        d.setLeftChild( b);
-        Node x = new Node('x');
-        d.setRightChild( x);
-        b.setParent(d);
-        Node six = new Node('6');
-        b.setLeftChild( six);
+        for (int i = 0; i < alphabet.length(); i++) {
+            char value = alphabet.charAt(i);
+            Node node = new Node(value);
+            nodes[i]=node;
+            if (value != '_') {
+                this.map.put(value, node);
+            }
+        }
+        for (int i = 0; i < alphabet.length(); i++) {
+            if (i > 0) {
+                nodes[i].setParent(nodes[((i - 1) / 2)]);
+            }
+            if (i < 31) {
+                nodes[i].setLeftChild(nodes[(2 * i) + 1]);
+                nodes[i].setRightChild(nodes[(2 * i) + 2]);
+            }
+        }
+        this.root = nodes[0];
 
-        six.setParent(b);
-
-        x.setParent(d);
-
-        k.setParent(n);
-        Node c = new Node('c');
-        k.setLeftChild(c );
-        Node y = new Node('y');
-        k.setRightChild( y);
-        c.setParent(k);
-        y.setParent(k);
-        m.setParent(t);
-        Node g = new Node('g');
-        m.setLeftChild( g);
-        Node o = new Node('o');
-        m.setRightChild( o);
-        g.setParent(m);
-        Node z = new Node('z');
-        g.setLeftChild( z);
-        Node q = new Node('q');
-        g.setRightChild( q);
-        q.setParent(g);
-        z.setParent(g);
-        Node seven = new Node('7');
-        z.setLeftChild(seven );
-        o.setParent(m);
-        seven.setParent(z);
-        Node oNullLeft = new Node();
-        o.setLeftChild(oNullLeft );
-        Node oNullRight = new Node();
-        o.setRightChild( oNullRight);
-        oNullLeft.setParent(o);
-        Node eight = new Node('8');
-        oNullLeft.setLeftChild(eight );
-        oNullRight.setParent(o);
-        eight.setParent(oNullLeft);
-        Node nine = new Node('9');
-        oNullRight.setLeftChild( nine);
-        nine.setParent(oNullRight);
-        Node zero = new Node('0');
-        oNullRight.setRightChild(zero );
-        zero.setParent(oNullRight);
-
-        e.setParent(root);
-        Node i = new Node('i');
-        e.setLeftChild( i);
-        Node a = new Node('a');
-        e.setRightChild( a);
-        i.setParent(e);
-        Node s = new Node('s');
-        i.setLeftChild(s );
-        Node u = new Node('u');
-        i.setRightChild(u);
-        a.setParent(e);
-        Node r = new Node('r');
-        a.setLeftChild( r);
-        Node w = new Node('w');
-        a.setRightChild(w );
-        s.setParent(i);
-        Node h = new Node('h');
-        s.setLeftChild( h);
-        Node v = new Node('v');
-        s.setRightChild( v);
-        u.setParent(i);
-        Node f = new Node('f');
-        u.setLeftChild( f);
-        f.setParent(u);
-        Node uNull = new Node();
-        u.setRightChild( uNull);
-        r.setParent(a);
-        Node l = new Node('l');
-        l.setParent(r);
-        r.setLeftChild(l );
-        Node rNull = new Node();
-        r.setRightChild(rNull );
-        w.setParent(a);
-        Node p = new Node('p');
-        p.setParent(w);
-        w.setLeftChild(p);
-        Node j = new Node('j');
-        w.setRightChild(j);
-        h.setParent(s);
-
-        Node five = new Node('5');
-        h.setLeftChild( five);
-        five.setParent(h);
-        Node four = new Node('4');
-        h.setRightChild( four);
-        four.setParent(h);
-        v.setParent(s);
-        Node vNull = new Node();
-        v.setLeftChild(vNull );
-        Node three = new Node('3');
-        v.setRightChild(three);
-        three.setParent(v);
-        uNull.setParent(u);
-        Node two = new Node('2');
-        uNull.setRightChild(two );
-        two.setParent(uNull);
-        rNull.setParent(r);
-
-        Node rNNull = new Node();
-        rNull.setRightChild( rNNull);
-        j.setParent(w);
-        Node one = new Node('1');
-        j.setRightChild(one);
-        one.setParent(j);
-
-        this.map.put('a',a);
-        this.map.put('b',b);
-        this.map.put('c',c);
-        this.map.put('d',d);
-        this.map.put('e',e);
-        this.map.put('f',f);
-        this.map.put('g',g);
-        this.map.put('h',h);
-        this.map.put('i',i);
-        this.map.put('j',j);
-        this.map.put('k',k);
-        this.map.put('l',l);
-        this.map.put('m',m);
-        this.map.put('n',n);
-        this.map.put('o',o);
-        this.map.put('p',p);
-        this.map.put('q',q);
-        this.map.put('r',r);
-        this.map.put('s',s);
-        this.map.put('t',t);
-        this.map.put('u',u);
-        this.map.put('v',v);
-        this.map.put('w',w);
-        this.map.put('x',x);
-        this.map.put('y',y);
-        this.map.put('z',z);
-        this.map.put('0',zero);
-        this.map.put('1',one);
-        this.map.put('2',two);
-        this.map.put('3',three);
-        this.map.put('4',four);
-        this.map.put('5',five);
-        this.map.put('6',six);
-        this.map.put('7',seven);
-        this.map.put('8',eight);
-        this.map.put('9',nine);
+        }
 
 
-
-
-    }
 
 
     // ESTE MÉTODO DEVE SER PREENCHIDO DE ACORDO COM O ENUNCIADO!
@@ -251,7 +102,7 @@ public class Translator {
             else{
 
 
-                return 0;
+                return ' ';
             }
         }
 
@@ -289,29 +140,6 @@ public class Translator {
 
     // ESTE MÉTODO DEVE SER PREENCHIDO DE ACORDO COM O ENUNCIADO!
     public LinkedList<String> getCodes() {
-////        private HashMap<Character, Node>   = new HashMap<>();
-//        LinkedList<String> codes = new LinkedList();
-//
-//        Node current = this.root;
-//        for (int i = 0; i < this.map.size(); i++) {
-////          System.out.print(current.getLeftChild().getValue());
-//            System.out.println(codes);
-//            if(!codes.contains(this.charToMorse(current.getLeftChild().getValue()))){
-//                codes.add(this.charToMorse((current.getLeftChild()).getValue()));
-//            }
-//            else if (!codes.contains(this.charToMorse(current.getRightChild().getValue()))){
-////                System.out.print(current.getLeftChild().getValue());
-//                codes.add(this.charToMorse(current.getRightChild().getValue()));
-//
-//            }
-//            else {
-//
-//                current = current.getLeftChild();
-////                System.out.println(current.getValue());
-//            }
-//
-//        }
-//    return codes;
 
         LinkedList<String> morse_list = new LinkedList<>();
         Queue<Node> queue = new LinkedList<Node>();
@@ -322,7 +150,7 @@ public class Translator {
 
             Node current = queue.poll();
 
-            if (current.getValue() != ' '){
+            if (current.getValue() != '_'){
                 morse_list.add(this.charToMorse(current.getValue()));
             }
 
